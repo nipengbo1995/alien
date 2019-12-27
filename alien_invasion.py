b23d6da6f639b3a11d3a1069d182c56ebe291f2c
nipengbo1995 <673242024@qq.com>
2019-12-27 16:39:28 +0800

    2019/12/27-1

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,29 +2,22 @@ import sys
 
 import pygame
 
-<<<<<<< HEAD
-=======
 from settings import Settings
 
->>>>>>> 2019/12/26-2
+from ship import Ship
+
 
 def run_game():
     # 初始化游戏并创建一个屏幕对象
     pygame.init()
-<<<<<<< HEAD
-    screen = pygame.display.set_mode((1200, 800))
-    pygame.display.set_caption("Alien Invasion")
-
-=======
     ai_settings = Settings()
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    # 设置背景色
-    bg_color = (230, 230, 230)
+    # 创建一艘飞船
+    ship = Ship(screen)
 
->>>>>>> 2019/12/26-2
     # 开始游戏的主循环
     while True:
 
@@ -32,13 +25,10 @@ def run_game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-
-<<<<<<< HEAD
-=======
         # 每次循环都重绘屏幕
-        screen.fill(bg_color)
+        screen.fill(ai_settings.bg_color)
+        ship.blitme()
 
->>>>>>> 2019/12/26-2
         # 让最近绘制的屏幕可见
         pygame.display.flip()
 
